dc9b9f5ca8afb72cd57b4aa0d2e698cda061641a
Hejociara <hejociarav@gmail.com>
2022-10-06 22:02:21 -0300

    "First commit"

--- src/Challenge.java
+++ src/Challenge.java
@@ -4,9 +4,12 @@ public class Challenge {
      */
     public static void main(String[] args) {
         String value = "#";
-        for (int i = 1; i <=5; i++){
+        for (int i = 1; i <=5; i++){ // 5, número de repetições
             System.out.println(value);
             value += "#";
+        }
+        for(String v = "#"; !v.equals("#####");v +="#"){ //Enquanto v for diferente
+            System.out.println(v);
         }
     }
 }
